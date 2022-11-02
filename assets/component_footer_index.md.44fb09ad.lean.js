74ee3251c09fe04545cf6ab9dc7b0da9bf1bfdf5
宏联搬砖工 <java_hongwx@163.com>
2022-11-02 17:22:21 +0800

    deploy

---
+++ assets/component_footer_index.md.44fb09ad.lean.js
@@ -0,0 +1,21 @@
+import{V as _}from"./framework.d640b77a.js";import{_ as m,z as h,A as f,e as p,G as r,I as n,J as e,L as i}from"./plugin-vue_export-helper.6ba15610.js";import"./framework.3728bfc1.js";const F={name:"component-doc",components:{"render-demo-0":function(){const{resolveComponent:t,createVNode:o,openBlock:s,createElementBlock:a}=_;function u(c,l){const k=t("hl-footer");return s(),a("div",null,[o(k)])}return{render:u,...{}}}()}},b='{"title":"footer \u9875\u811A","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5982\u4F55\u4F7F\u7528","slug":"\u5982\u4F55\u4F7F\u7528"},{"level":2,"title":"\u8865\u5145\u8BF4\u660E","slug":"\u8865\u5145\u8BF4\u660E"}],"relativePath":"component/footer/index.md"}',g=n("h1",{id:"footer-\u9875\u811A",tabindex:"-1"},[e("footer \u9875\u811A "),n("a",{class:"header-anchor",href:"#footer-\u9875\u811A","aria-hidden":"true"},"#")],-1),x=n("h2",{id:"\u5982\u4F55\u4F7F\u7528",tabindex:"-1"},[e("\u5982\u4F55\u4F7F\u7528 "),n("a",{class:"header-anchor",href:"#\u5982\u4F55\u4F7F\u7528","aria-hidden":"true"},"#")],-1),B=n("div",{class:"language-vue line-numbers-mode"},[n("pre",null,[n("code",null,[e(`
+`),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),e("template")]),n("span",{class:"token punctuation"},">")]),e(`
+  `),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),e("hl-footer")]),n("span",{class:"token punctuation"},">")]),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),e("hl-footer")]),n("span",{class:"token punctuation"},">")]),e(`
+`),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),e("template")]),n("span",{class:"token punctuation"},">")]),e(`
+`),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),e("style")]),e(),n("span",{class:"token attr-name"},"scoped"),n("span",{class:"token punctuation"},">")]),n("span",{class:"token style"},[n("span",{class:"token language-css"},[e(`
+    `),n("span",{class:"token selector"},".hl-footer-block"),e(),n("span",{class:"token punctuation"},"{"),e(`
+        `),n("span",{class:"token property"},"display"),n("span",{class:"token punctuation"},":"),e("flex"),n("span",{class:"token punctuation"},";"),e(`
+        `),n("span",{class:"token property"},"justify-content"),n("span",{class:"token punctuation"},":"),e("center"),n("span",{class:"token punctuation"},";"),e(`
+    `),n("span",{class:"token punctuation"},"}"),e(`
+`)])]),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),e("style")]),n("span",{class:"token punctuation"},">")]),e(`
+`)])])],-1),v=n("h2",{id:"\u8865\u5145\u8BF4\u660E",tabindex:"-1"},[e("\u8865\u5145\u8BF4\u660E "),n("a",{class:"header-anchor",href:"#\u8865\u5145\u8BF4\u660E","aria-hidden":"true"},"#")],-1),y=n("p",null,[e("\u70B9\u51FB"),n("code",null,"\u5B8F\u8054\u79D1\u6280\u6821\u56ED\u670D\u52A1"),e(",\u5C06\u4F1A\u5F39\u51FA\u5B8F\u8054\u79D1\u6280\u7684\u5BA2\u670D\u516C\u4F17\u53F7\u4E8C\u7EF4\u7801")],-1);function E(t,o,s,a,u,d){const c=i("render-demo-0"),l=i("demo");return h(),f("div",null,[g,x,p(l,{sourceCode:`
+<template>
+  <hl-footer></hl-footer>
+</template>
+<style scoped>
+    .hl-footer-block {
+        display:flex;
+        justify-content:center;
+    }
+</style>
+`},{highlight:r(()=>[B]),default:r(()=>[p(c)]),_:1}),v,y])}var D=m(F,[["render",E]]);export{b as __pageData,D as default};
